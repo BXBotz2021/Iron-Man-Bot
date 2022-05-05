85efef178a1150cc1c97501d012c25c0a45d311c
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-05-05 20:50:08 +0530

    Add files via upload

---
+++ plugins/commands.py
@@ -0,0 +1,106 @@
+import os
+import logging
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from utils import Media, get_file_details, get_size
+logger = logging.getLogger(__name__)
+
+@Client.on_message(filters.command("start"))
+async def start(bot, cmd):
+    await cmd.reply_text(
+        START_MSG,
+        parse_mode="Markdown",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕", url="http://t.me/TGAntiServiceBot?startgroup=true")
+                ],
+                [
+                    InlineKeyboardButton("🔍 sᴇᴀʀᴄʜ", switch_inline_query_current_chat=''),
+                    InlineKeyboardButton("ᴀʙᴏᴜᴛ 🔰", callback_data="about")
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(filters.command('channel') & filters.user(ADMINS))
+async def channel_info(bot, message):
+    """Send basic information of channel"""
+    if isinstance(CHANNELS, (int, str)):
+        channels = [CHANNELS]
+    elif isinstance(CHANNELS, list):
+        channels = CHANNELS
+    else:
+        raise ValueError("Unexpected type of CHANNELS")
+
+    text = '📑 **Indexed channels/groups**\n'
+    for channel in channels:
+        chat = await bot.get_chat(channel)
+        if chat.username:
+            text += '\n@' + chat.username
+        else:
+            text += '\n' + chat.title or chat.first_name
+
+    text += f'\n\n**Total:** {len(CHANNELS)}'
+
+    if len(text) < 4096:
+        await message.reply(text)
+    else:
+        file = 'Indexed channels.txt'
+        with open(file, 'w') as f:
+            f.write(text)
+        await message.reply_document(file)
+        os.remove(file)
+
+
+@Client.on_message(filters.command('total') & filters.user(ADMINS))
+async def total(bot, message):
+    """Show total files in database"""
+    msg = await message.reply("⏳ ᴘʀᴏᴄᴇssɪɴɢ.....", quote=True)
+    try:
+        total = await Media.count_documents()
+        await msg.edit(f'📁 sᴀᴠᴇᴅ ꜰɪʟᴇs: {total}')
+    except Exception as e:
+        logger.exception('Failed to check total files')
+        await msg.edit(f'Error: {e}')
+
+
+@Client.on_message(filters.command('log') & filters.user(ADMINS))
+async def log_file(bot, message):
+    """Send log file"""
+    try:
+        await message.reply_document('TelegramBot.log')
+    except Exception as e:
+        await message.reply(str(e))
+
+
+@Client.on_message(filters.command('delete') & filters.user(ADMINS))
+async def delete(bot, message):
+    """Delete file from database"""
+    reply = message.reply_to_message
+    if reply and reply.media:
+        msg = await message.reply("⏳ ᴘʀᴏᴄᴇssɪɴɢ.....", quote=True)
+    else:
+        await message.reply('Reply to file with /delete which you want to delete', quote=True)
+        return
+
+    for file_type in ("document", "video", "audio"):
+        media = getattr(reply, file_type, None)
+        if media is not None:
+            break
+    else:
+        await msg.edit('ᴛʜɪs ɪs ɴᴏᴛ sᴜᴘᴘᴏʀᴛᴇᴅ ꜰɪʟᴇ ꜰᴏʀᴍᴀᴛ 😐')
+        return
+
+    result = await Media.collection.delete_one({
+        'file_name': media.file_name,
+        'file_size': media.file_size,
+        'mime_type': media.mime_type
+    })
+    if result.deleted_count:
+        await msg.edit('ꜰɪʟᴇ ɪs sᴜᴄᴄᴇssꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ꜰʀᴏᴍ ᴅᴀᴛᴀʙᴀsᴇ 🗑')
+    else:
+        await msg.edit('ꜰɪʟᴇ ɴᴏᴛ ꜰᴏᴜɴᴅ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ 😕')
---
+++ plugins/pm_filter.py
@@ -0,0 +1,376 @@
+#Kanged From @TroJanZheX
+from info import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, BUTTON
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from pyrogram import Client, filters
+import re
+import os
+import asyncio
+import imdb
+from utils import get_filter_results, get_file_details
+import pytz, datetime
+import requests
+
+API_KEY = os.environ.get("API_KEY")
+
+BUTTONS = {}
+BOT = {}
+SEND_CHANNEL = int(os.environ.get("SEND_CHANNEL"))
+SEND_USERNAME = os.environ.get("SEND_USERNAME")
+FILE_CAP = """<b>[{}]{}
+
+{} {} ✨</b>
+
+🔰 ʙᴇᴄᴀᴜsᴇ ᴏꜰ ᴄᴏᴘʏʀɪɢʜᴛ ᴛʜɪs ꜰɪʟᴇ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ꜰʀᴏᴍ ʜᴇʀᴇ ᴡɪᴛʜɪɴ 10 ᴍɪɴᴜᴛᴇs
+sᴏ ꜰᴏʀᴡᴀʀᴅ ɪᴛ ᴛᴏ ᴀɴʏᴡʜᴇʀᴇ ʙᴇꜰᴏʀᴇ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ!
+
+<i>കോപ്പിറൈറ്റ് ഉള്ളതുകൊണ്ട് ഈ ഫയൽ 10 മിനിറ്റിനുള്ളിൽ ഇവിടെനിന്നും ഡിലീറ്റ് ആകുന്നതാണ്
+അതുകൊണ്ട് ഇവിടെ നിന്നും മറ്റെവിടെക്കെങ്കിലും മാറ്റിയതിന് ശേഷം ഡൗൺലോഡ് ചെയ്യുക!</i>
+
+<b>✅ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : {}</b>
+"""
+autofiltercaption = """<b>🎬 Title : {}</b>
+                            
+⏱ Duration : {} minutes
+🌟 Rating : {}/10
+📆 Release : {}
+🎭 Genres : {}
+
+<b>👤 Requested BY : {}
+✅ Powered BY : {}</b>"""
+
+MAX_LIST_ELM = int(5)
+def list_to_str(k):
+    if not k:
+        return "N/A"
+    elif len(k) == 1:
+        return str(k[0])
+    elif MAX_LIST_ELM:
+        k = k[:int(MAX_LIST_ELM)]
+        return ' '.join(f'{elem}, ' for elem in k)
+    else:
+        return ' '.join(f'{elem}, ' for elem in k)
+    
+@Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
+async def group(client, message):
+    if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
+        return
+    if 2 < len(message.text) < 50:    
+        btn = []
+        search = message.text
+        nyva=BOT.get("username")
+        if not nyva:
+            botusername=await client.get_me()
+            nyva=botusername.username
+            BOT["username"]=nyva
+        files = await get_filter_results(query=search)
+        if files:
+            for file in files:
+                file_id = file.file_id
+                filename = f"[{get_size(file.file_size)}] {file.file_name}"
+                btn.append(
+                    [InlineKeyboardButton(text=f"📁 {filename}", callback_data=f"subinps#{file_id}")]
+                )
+        else:
+            google_keyword = search.replace(" ", "+")
+            msg = await message.reply_text(text="""
+                <b>Hello {} I could not find the movie you asked for 🥲
+
+Click on buttons below to search on google or IMDb</b>
+                  """.format(message.from_user.mention),
+                 reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('🌟 IMDB 🌟', url='https://imdb.com'),
+                        InlineKeyboardButton('⚡ GOOGLE ⚡️', url=f'https://www.google.com/search?q={google_keyword}')
+                    ],
+                    [
+                        InlineKeyboardButton("😐 Nonsense 😐", callback_data="no_results")
+                    ]
+                ]
+            )
+        )
+            await asyncio.sleep(20)
+            await message.delete()
+            await msg.delete()
+        if not btn:
+            return
+
+        if len(btn) > 10: 
+            btns = list(split_list(btn, 10)) 
+            keyword = f"{message.chat.id}-{message.id}"
+            BUTTONS[keyword] = {
+                "total" : len(btns),
+                "buttons" : btns
+            }
+        else:
+            buttons = btn
+            if BUTTON:
+                buttons.append([InlineKeyboardButton(text="ᴄʟᴏsᴇ 🔒",callback_data="close")])
+
+            search = message.text
+            ia = imdb.IMDb()
+            movies = ia.search_movie(search)
+            if movies:
+                id = movies[0].getID()
+                movie = ia.get_movie(id)
+                title = movie['title']
+                year = movie['year']
+                rating = movie['rating']
+                plo = movie['plot']
+                plot = plo[0]
+                runtime = list_to_str(movie['runtime'])
+                genre = list_to_str(movie['genres'])
+                language = movie['languages']
+                director = movie['director']
+                stars = list_to_str(movie['cast'])
+                fileid = movies[0].get_fullsizeURL()
+                mention = message.from_user.mention
+                chat = message.chat.title
+                dell = await message.reply_photo(photo=fileid, caption=autofiltercaption.format(title, runtime, rating, year, genre, stars, plot, mention, chat), reply_markup=InlineKeyboardMarkup(buttons))  
+                await asyncio.sleep(600)
+                await dell.delete()
+   
+            else:
+                del1 = await message.reply_text(f"""<b>Hey 👋 {message.from_user.mention} 😍
+
+📁 Found ✨  Files For Your Query : {search} 👇</b>""", 
+                reply_markup=InlineKeyboardMarkup(buttons))
+                await asyncio.sleep(600)
+                await del1.delete()
+               
+        btns = list(split_list(btn, 10)) 
+        keyword = f"{message.chat.id}-{message.id}"
+        BUTTONS[keyword] = {
+            "total" : len(btns),
+            "buttons" : btns
+        }
+        data = BUTTONS[keyword]
+        buttons = data['buttons'][0].copy()
+
+        buttons.append(
+            [InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_0_{keyword}")]
+        )
+        if BUTTON:
+            buttons.append([InlineKeyboardButton(text="ᴄʟᴏsᴇ 🔒",callback_data="close")])
+         
+        search = message.text
+        ia = imdb.IMDb()
+        movies = ia.search_movie(search)
+        if movies:
+            id = movies[0].getID()
+            movie = ia.get_movie(id)
+            title = movie['title']
+            year = movie['year']
+            rating = movie['rating']
+            plo = movie['plot']
+            plot = plo[0]
+            runtime = list_to_str(movie['runtime'])
+            genre = list_to_str(movie['genres'])
+            language = movie['languages']
+            director = movie['director']
+            stars = list_to_str(movie['cast'])
+            fileid = movies[0].get_fullsizeURL()
+            mention = message.from_user.mention
+            chat = message.chat.title
+            del2 = await message.reply_photo(photo=fileid, caption=autofiltercaption.format(title, runtime, rating, year, genre, stars, plot, mention, chat), reply_markup=InlineKeyboardMarkup(buttons))  
+            await asyncio.sleep(600)
+            await del2.delete()
+   
+        else:
+            del3 = await message.reply_text(f"""<b>Hey 👋 {message.from_user.mention} 😍
+
+📁 Found ✨  Files For Your Query : {search} 👇</b>""", 
+            reply_markup=InlineKeyboardMarkup(buttons))
+            await asyncio.sleep(600)
+            await del3.delete()
+     
+
+def get_size(size):
+    """Get size in readable format"""
+
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
+def split_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]          
+
+
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    clicked = query.from_user.id
+    try:
+        typed = query.message.reply_to_message.from_user.id
+    except:
+        typed = query.from_user.id
+        pass
+    if (clicked == typed):
+
+        if query.data.startswith("next"):
+            ident, index, keyword = query.data.split("_")
+            try:
+                data = BUTTONS[keyword]
+            except KeyError:
+                await query.answer("You are using this for one of my old message, please send the request again.",show_alert=True)
+                return
+
+            if int(index) == int(data["total"]) - 2:
+                buttons = data['buttons'][int(index)+1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("⏪ BACK", callback_data=f"back_{int(index)+1}_{keyword}")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="ᴄʟᴏsᴇ 🔒",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+            else:
+                buttons = data['buttons'][int(index)+1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("⏪ BACK", callback_data=f"back_{int(index)+1}_{keyword}"),InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)+1}_{keyword}")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="ᴄʟᴏsᴇ 🔒",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+
+        elif query.data.startswith("back"):
+            ident, index, keyword = query.data.split("_")
+            try:
+                data = BUTTONS[keyword]
+            except KeyError:
+                await query.answer("You are using this for one of my old message, please send the request again.",show_alert=True)
+                return
+
+            if int(index) == 1:
+                buttons = data['buttons'][int(index)-1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)-1}_{keyword}")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="ᴄʟᴏsᴇ 🔒",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return   
+            else:
+                buttons = data['buttons'][int(index)-1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("⏪ BACK", callback_data=f"back_{int(index)-1}_{keyword}"),InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)-1}_{keyword}")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="ᴄʟᴏsᴇ 🔒",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+
+
+        elif query.data.startswith("subinps"):
+            ident, file_id = query.data.split("#")
+            filedetails = await get_file_details(file_id)
+            for files in filedetails:
+                title = files.file_name
+                size=get_size(files.file_size)
+                f_caption=files.caption
+                mention = query.from_user.mention
+                chat = query.message.chat.title
+                m = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+                time = m.hour
+
+                if time < 12:
+                    get="Good Morning"
+                elif time < 15:
+                    get="Good Afternoon"
+                elif time < 20:
+                    get="Good Evening"
+                else:
+                    get="Good Night"
+
+                if CUSTOM_FILE_CAPTION:
+                    try:
+                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption, time=get)
+                    except Exception as e:
+                        print(e)
+                        f_caption=f_caption
+                if f_caption is None:
+                    f_caption = f"""<b>[{size}]{title}
+
+{get} {query.from_user.mention} ✨</b>
+
+🔰 ʙᴇᴄᴀᴜsᴇ ᴏꜰ ᴄᴏᴘʏʀɪɢʜᴛ ᴛʜɪs ꜰɪʟᴇ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ꜰʀᴏᴍ ʜᴇʀᴇ ᴡɪᴛʜɪɴ 10 ᴍɪɴᴜᴛᴇs
+sᴏ ꜰᴏʀᴡᴀʀᴅ ɪᴛ ᴛᴏ ᴀɴʏᴡʜᴇʀᴇ ʙᴇꜰᴏʀᴇ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ!
+
+<i>കോപ്പിറൈറ്റ് ഉള്ളതുകൊണ്ട് ഈ ഫയൽ 10 മിനിറ്റിനുള്ളിൽ ഇവിടെനിന്നും ഡിലീറ്റ് ആകുന്നതാണ്
+അതുകൊണ്ട് ഇവിടെ നിന്നും മറ്റെവിടെക്കെങ്കിലും മാറ്റിയതിന് ശേഷം ഡൗൺലോഡ് ചെയ്യുക!</i>
+
+<b>✅ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : {query.chat.title}</b>"""
+                buttons = [
+                    [
+                        InlineKeyboardButton('🔗 Join Here 😍🔥', url='https://t.me/BX_Botz')
+                    ]
+                    ]
+                
+                await query.answer()
+                filess = await client.send_cached_media(
+                    chat_id=SEND_CHANNEL,
+                    file_id=file_id,
+                    caption=FILE_CAP.format(size, title, get, mention, chat),
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                    )
+                
+                
+                humm = [[
+                        InlineKeyboardButton("📥 Download Link 📥", url=f"{filess.link}")
+                        ],[
+                        InlineKeyboardButton("⚠️ Can't Access❓ Click Here ⚠️", url=f"https://t.me/{SEND_USERNAME}")
+                        ]]
+                reply_markup=InlineKeyboardMarkup(humm)
+                msg1 = await query.message.reply(text=f"""Hey 👋 {query.from_user.mention} 
+
+📫 Yᴏʀ Fɪʟᴇ ɪꜱ Rᴇᴀᴅʏ 👇
+
+📂 Mᴏᴠɪᴇ Nᴀᴍᴇ : {title}
+
+⚙️ Mᴏᴠɪᴇ Sɪᴢᴇ : {size}
+
+<code>This message will be deleted after 10 minutes</code>""", reply_markup=reply_markup)
+                await asyncio.sleep(600)
+                await filess.delete()
+                await msg1.delete()
+                
+                return  
+        
+
+
+        elif query.data == "pages":
+            await query.answer("No Use", show_alert=False)
+        elif query.data == "close":
+            try:
+                await query.message.reply_to_message.delete()
+                await query.message.delete()
+            except:
+                await query.message.delete()
+        elif query.data == "no_results":
+            await query.answer("സിനിമ ലഭിക്കണം എങ്കിൽ താങ്കൾ ഗൂഗിൾ നോക്കി സിനിമയുടെ correct spelling ഇവിടെ send ചെയ്യുക എങ്കിലേ താങ്കൾ ഉദ്ദേശിക്കുന്ന സിനിമ എനിക്ക് അയച്ചു തരാൻ കഴിയുകയുള്ളു 😊", show_alert=True)
+                
+    else:
+        await query.answer("Not For You !",show_alert=True)
