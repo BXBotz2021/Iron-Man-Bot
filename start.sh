632ecf6e53279dd458ec9391c6041295c071dc00
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-05-08 10:20:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repository"
-git clone https://github.com/BXBotz2021/Iron-Man-Bot.git
+git clone https://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@github.com/BXBotz2021/Iron-Man-Bot.git
 cd Iron-Man-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
